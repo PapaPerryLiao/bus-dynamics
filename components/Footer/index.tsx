d2be7659d78f5a7c35116d9d3eca8afcc22956dd
PerryLiao <perry.liao@area02.com>
2021-11-28 23:41:29 +0800

    feat: Footer component

---
+++ components/Footer/index.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <div className="container">
+      Copyright | Developed by Perry Liao | Data provided by TDX API
+      <style jsx>{`
+        .container {
+          width: 100%;
+          height: 30px;
+          line-height: 30px;
+          background-color: gray;
+          text-align: center;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default Footer;
