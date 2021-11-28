619505f67f0384882c12ce92b91f8d11b19fcaea
PerryLiao <perry.liao@area02.com>
2021-11-29 01:48:01 +0800

    feat: use url rewrite

--- next.config.js
+++ next.config.js
@@ -2,16 +2,16 @@
 
 module.exports = {
   reactStrictMode: true,
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/api/tdx/:path*",
-  //       destination: "https://ptx.transportdata.tw/:path*",
-  //     },
-  //     {
-  //       source: "/api/motc/:path*",
-  //       destination: "https://link.motc.gov.tw/:path*",
-  //     },
-  //   ];
-  // },
+  async rewrites() {
+    return [
+      {
+        source: "/api/tdx/:path*",
+        destination: "https://ptx.transportdata.tw/:path*",
+      },
+      {
+        source: "/api/motc/:path*",
+        destination: "https://link.motc.gov.tw/:path*",
+      },
+    ];
+  },
 };
