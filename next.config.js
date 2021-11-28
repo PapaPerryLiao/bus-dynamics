26eb267b84787f9bd85885a850755d38a8ee4cf4
PerryLiao <perry.liao@area02.com>
2021-11-28 23:50:09 +0800

    feat: config & url rewrite setting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 module.exports = {
   reactStrictMode: true,
-}
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/tdx/:path*",
+  //       destination: "https://ptx.transportdata.tw/:path*",
+  //     },
+  //     {
+  //       source: "/api/motc/:path*",
+  //       destination: "https://link.motc.gov.tw/:path*",
+  //     },
+  //   ];
+  // },
+};
